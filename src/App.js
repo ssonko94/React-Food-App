20560397074818088fc79f6f6a86f2e8cb3a4eb1
ssonko94 <ssonkojimmy@gmail.com>
2021-06-02 17:56:10 +0300

    render the cart in the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import Cart from "./Cart/Cart";
 import Header from "./Layout/Header";
 import Meals from "./Meals/Meals";
 
 function App() {
   return (
     <>
+      <Cart />
       <Header />
       <main>
-        <Meals/>
+        <Meals />
       </main>
     </>
   );
