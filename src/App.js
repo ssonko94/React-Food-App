f050109c9b386921917165c3f7c5cd9ab06b6697
ssonko94 <ssonkojimmy@gmail.com>
2021-06-02 20:59:50 +0300

    wrap the app with the cart context

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Cart from "./Components/Cart/Cart";
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -15,13 +16,13 @@ function App() {
     setCartIsShown(false);
   };
   return (
-    <>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHeader} />}
       <Header onShowCart={showCartHeader} />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
 
