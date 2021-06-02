826b5f0ae57be857f75760bee73b2e5c10475d55
ssonko94 <ssonkojimmy@gmail.com>
2021-06-02 15:44:34 +0300

    add the meals component to the app componet in a main tag component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Header from "./Layout/Header";
+import Meals from "./Meals/Meals";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Meals/>
+      </main>
     </>
   );
 }
