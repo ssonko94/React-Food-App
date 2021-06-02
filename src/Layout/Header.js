0a947a623a1930884a97c07b7bb323a80f3e6206
ssonko94 <ssonkojimmy@gmail.com>
2021-06-02 15:13:19 +0300

    import the header cart button into the header component

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,15 +1,16 @@
 import React from "react";
-import classes from "./Header.module.css"
+import classes from "./Header.module.css";
 import mealsImage from "../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
         <h1>Foodiez meals</h1>
-        <button>Cart</button>
+        <HeaderCartButton>Cart</HeaderCartButton>
       </header>
-      <div className={classes['main-image']}>
+      <div className={classes["main-image"]}>
         <img src={mealsImage} alt="a table full of delicious food!" />
       </div>
     </>
