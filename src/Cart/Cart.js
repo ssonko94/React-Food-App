a63c4e6e968f5664d52e2b2392d1ed1cdf71e5c9
ssonko94 <ssonkojimmy@gmail.com>
2021-06-02 17:55:32 +0300

    add button to close the cart list

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -1,29 +1,28 @@
 import React from "react";
+import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 
 const Cart = () => {
   const cartItems = (
-    <ul className={classes['cart-items']}>
+    <ul className={classes["cart-items"]}>
       {[{ id: "c1", name: "sushi", amount: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+        <li key={item.id}>{item.name}</li>
       ))}
     </ul>
   );
 
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div className={classes.total}>
-          <span>Total Amount</span>
-          <span>35.62</span>
+        <span>Total Amount</span>
+        <span>35.62</span>
       </div>
       <div className={classes.actions}>
-      <button className={classes['button--alt']}>Close</button>
-      <button className={classes.button}>Order</button>
-
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Order</button>
       </div>
-      
-    </div>
+    </Modal>
   );
 };
 
