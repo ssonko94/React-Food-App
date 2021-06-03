4a112d09b6b95078e6280eb971e72781a0882ed3
ssonko94 <ssonkojimmy@gmail.com>
2021-06-03 10:47:37 +0300

    consue the add and remove items functions from the context to make the buttons function

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -7,13 +7,17 @@ import CartItem from "./CartItem";
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
 
-  const totalAmount = ` ${cartCtx.totalAmount.toFixed(2)}`;
+  const totalAmount = ` $${cartCtx.totalAmount.toFixed(2)}`;
 
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemAddHandler = (item) => {};
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
 
-  const cartItemRemovedHandler = (id) => {};
+  const cartItemRemovedHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
